9d56dad9f1a334bd8651ab8066e632ebaa20236d
yjy1 <318277446@qq.com>
2023-02-20 13:42:25 +0800

    tag_路由-获取请求参数

---
+++ 028-路由-获取请求参数/route.js
@@ -0,0 +1,27 @@
+const fs = require('fs')
+
+function render(res,path,statusCode){
+    res.writeHead(statusCode,{"Content-Type":"text/html;charset=utf-8"})
+    res.write(fs.readFileSync(path),'utf-8')
+    res.end()
+}
+ 
+const route = {
+    '/login':(req,res)=>{
+        render(res,'./static/login.html',200)
+        // res.writeHead(200,{"Content-Type":"text/html;charset=utf-8"})
+        // res.write(fs.readFileSync('./static/login.html'),'utf-8')
+    },
+    '/home':(req,res)=>{
+        render(res,'./static/home.html',200)
+        // res.writeHead(200,{"Content-Type":"text/html;charset=utf-8"})
+        // res.write(fs.readFileSync('./static/home.html'),'utf-8')
+    },
+    '/404':(req,res)=>{
+        render(res,'./static/404.html',404)
+        // res.writeHead(404,{"Content-Type":"text/html;charset=utf-8"})
+        // res.write(fs.readFileSync('./static/404.html'),'utf-8')
+    }
+}
+
+module.exports  = route
---
+++ 028-路由-获取请求参数/server.js
@@ -0,0 +1,27 @@
+const http = require('http')
+ 
+const Router = {}
+ 
+function use(obj){
+    Object.assign(Router,obj)
+}
+
+function start(){
+    http.createServer((req,res)=>{
+        const myURL = new URL(req.url,'http://127.0.0.1')
+        console.log(myURL.pathname)
+        // route(myURL.pathname,res)
+        try {
+            Router[myURL.pathname](req,res)
+        } catch (error) {
+            Router['/404'](req,res)
+        }
+    
+        res.end()
+    }).listen(3000,()=>{
+        console.log('server start')
+    })
+}
+
+exports.start = start
+exports.use = use
