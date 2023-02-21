0d1f0e568061de9f164c4abc280c588806bea37d
yjy1 <318277446@qq.com>
2023-02-21 17:48:23 +0800

    tag_登录鉴权-Cookie与Sessoin-3

---
+++ 043-登录鉴权-Cookie与Sessoin-3/server/routes/index.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  // 判断 req.session.user
+  
+    res.render('index', { title: 'Express' });
+  
+});
+
+module.exports = router;
