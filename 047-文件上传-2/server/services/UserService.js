afe240d9571e39a649c25d87c2ed51b6fe879f7c
yjy1 <318277446@qq.com>
2023-02-22 12:36:43 +0800

    tag_文件上传-2

---
+++ 047-文件上传-2/server/services/UserService.js
@@ -0,0 +1,35 @@
+const UserModel = require("../model/UserModel")
+
+const UserService = {
+    addUser: (username,password,age,avatar) => {
+        UserModel.create({
+            username,
+            password,
+            age,
+            avatar
+        }).then(data => {
+            console.log(data)
+        })
+    },
+    updateUser: (req,username, password, age)=>{
+        UserModel.updateOne({ _id: req.params.id }, {
+            username
+        }).then(data => {
+            console.log(data)
+        })
+    },
+    deleteUser:(req)=>{
+        UserModel.deleteOne({ _id: req.params.id }).then(data => {
+            console.log(data)
+        })
+    },
+    getUser:(page,limit )=>{
+      return  UserModel.find({},['username','age']).sort({age:-1})
+          .skip((page - 1) * limit).limit(limit)
+    },
+    checkUser:(username,password)=>{
+        return UserModel.find({ username: username, password: password })
+    }
+}
+
+module.exports = UserService
